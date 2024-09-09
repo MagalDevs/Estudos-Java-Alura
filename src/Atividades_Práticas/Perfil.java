384995a5c00a92db670f2b8b2081bf9ddb694093
MagalDevs <danielgalfla12@gmail.com>
2024-09-08 23:08:07 -0300

    primeiras atividades

---
+++ src/Atividades_Práticas/Perfil.java
@@ -0,0 +1,8 @@
+package Atividades_Práticas;
+
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Daniel");
+        System.out.println("Tudo Bem?");
+    }
+}
---
+++ src/Atividades_Práticas/Subtracao.java
@@ -0,0 +1,7 @@
+package Atividades_Práticas;
+
+public class Subtracao {
+    public static void main(String[] args) {
+        System.out.println(10 - 5);
+    }
+}
