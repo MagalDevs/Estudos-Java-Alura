315d5635300bc9d2b91b5e39b720afa320de4725
MagalDevs <danielgalfla12@gmail.com>
2024-12-06 20:11:19 -0300

    Desafio final

---
+++ src/java_Trabalhando_Com_Listas_e_Colecoes_de_Dados/praticas/desafioFinal/classes/Produtos.java
@@ -0,0 +1,36 @@
+package java_Trabalhando_Com_Listas_e_Colecoes_de_Dados.praticas.desafioFinal.classes;
+
+public class Produtos implements Comparable<Produtos>{
+    String nome;
+    double preco;
+
+    public Produtos(double preco, String nome) {
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return  this.nome + " - " +
+                "R$ " + this.preco ;
+    }
+
+    @Override
+    public int compareTo(Produtos o) {
+        if (this.preco > o.preco){
+            return 1;
+        }
+        if (this.preco < o.preco){
+            return -1;
+        }
+        return 0;
+    }
+}
